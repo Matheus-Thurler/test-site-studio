2c16444daa0e385de36dca155be2633fe304105e
Matheus Thurler <matheusthurlernf@gmail.com>
2025-05-23 10:54:13 -0300

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -102,7 +102,7 @@ export default defineNuxtConfig({
   i18n: {
     locales: [
       { code: 'en', name: 'English', language: 'en-US' },
-      { code: 'pt-br', name: 'Portuguese', language: 'pt-BR' },
+      { code: 'ptbr', name: 'Portuguese', language: 'pt-BR' },
     ],
     detectBrowserLanguage: {
       useCookie: true,
