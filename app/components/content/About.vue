caaf7280d4063a62eed8c4a40fd230781a223eff
matheus <matheusthurlernf@gmail.com>
2025-06-24 10:52:51 -0300

    update

--- app/components/content/About.vue
+++ app/components/content/About.vue
@@ -54,28 +54,28 @@ const stack = await queryCollection('stack').first()
           />
         </p>
       </div>
-      <div class="flex flex-wrap gap-4">
-        <SpotlightCard
-          v-for="item in stack!.items"
-          :key="item.name"
-          white
-        >
-          <NuxtLink
-            :to="item.link"
-            target="_blank"
-            :aria-label="item.name + ' link'"
-            class="flex gap-2 p-6"
-          >
-            <UIcon
-              :name="item.icon"
-              class="size-9"
-              :font-controlled="false"
-              :alt="item.name + ' logo'"
-              :aria-label="item.name + ' logo'"
-            />
-          </NuxtLink>
-        </SpotlightCard>
-      </div>
+<!--      <div class="flex flex-wrap gap-4">-->
+<!--        <SpotlightCard-->
+<!--          v-for="item in stack!.items"-->
+<!--          :key="item.name"-->
+<!--          white-->
+<!--        >-->
+<!--          <NuxtLink-->
+<!--            :to="item.link"-->
+<!--            target="_blank"-->
+<!--            :aria-label="item.name + ' link'"-->
+<!--            class="flex gap-2 p-6"-->
+<!--          >-->
+<!--            <UIcon-->
+<!--              :name="item.icon"-->
+<!--              class="size-9"-->
+<!--              :font-controlled="false"-->
+<!--              :alt="item.name + ' logo'"-->
+<!--              :aria-label="item.name + ' logo'"-->
+<!--            />-->
+<!--          </NuxtLink>-->
+<!--        </SpotlightCard>-->
+<!--      </div>-->
     </div>
   </section>
 </template>
