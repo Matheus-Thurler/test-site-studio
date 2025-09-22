22818680113829ff51747019d9b6fae46a11d7c5
Matheus-Thurler <44201662+Matheus-Thurler@users.noreply.github.com>
2025-09-22 19:42:46 +0000

    Deploying to gh-pages from @ Matheus-Thurler/test-site-studio@65f84bf18d8a7fed9ba5f3899d1d9bc389b68e63 🚀

---
+++ _nuxt/BRWtRxYx.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./DlAUqK2U.js";import{c as o,d as s,a6 as t}from"./DKvLUe34.js";const n={};function a(e,c){return s(),o("tr",null,[t(e.$slots,"default")])}const f=Object.assign(r(n,[["render",a]]),{__name:"ProseTr"});export{f as default};
